f307e57f88482c171f6653b1d4cc1b81f6b81dc4
Pete S <petess@gmail.com>
2024-06-07 11:07:08 +1000

    first commit

---
+++ goXML_Test_1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/grokify/go-wordpressxml"
+)
+
+func main() {
+	inFileName := flag.String("infile", "", "The name of the XML file to convert")
+	outFileName := flag.String("outfile", "", "The name of the HTML file to export to")
+
+	flag.Parse()
+
+	wp := wordpressxml.NewWordPressXML()
+
+	if *inFileName == "" {
+		panic("an input XML file must be specified")
+	}
+
+	if *outFileName == "" {
+		panic("an output HTML file must be specified")
+	}
+
+	err := wp.ReadFile(*inFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	wp.ItemsToHTML(*outFileName)
+}
